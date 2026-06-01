de03a085b8c549a06890ad0beae401fa020a75a3
Александра Рудак <rudak_a@ItTopNSK.local>
2026-06-01 20:53:00 +0700

    create dashboard metrics

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import FocusInput from "./FocusInput";
-import FilterList from "./FilterList";
+import React, {useState, useMemo, useEffect} from "react";
+import { generateMockData } from "./mockData";
+import DashboardMetrics from './DashboardMetrics';
 
 function App() {
-  const DATA = [
-    {id: 1, name: 'A'},
-    {id: 2, name: 'Ab'},
-    {id: 3, name: 'Bbab'},
-    {id: 4, name: 'Caba'}
-  ]
+  const [rawData, setRawData] = useState(()=>generateMockData());
+
   return (
     <>
-     <FocusInput />
-     <FilterList users={DATA} />
+    <DashboardMetrics filteredData={rawData} />
     </>
   )
 }
